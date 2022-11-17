e46aa71580b3bacb6c716115e2b9fbd36fc9cd34
Kostya-59benz <razerresolution@gmail.com>
2022-11-17 23:22:24 +0200

    first commit

--- uaMedia/urls.py
+++ uaMedia/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import MovieView, MovieDetailView, AddReview, ActorView, FilterMoviesView, JsonFilterMoviesView, AddStarRating
+from .views import MovieView, MovieDetailView, AddReview, ActorView, FilterMoviesView, JsonFilterMoviesView, AddStarRating, Search
 
 
 urlpatterns = [
     path('',MovieView.as_view()),
     path('filter/', FilterMoviesView.as_view(), name='filter'),
+    path('search/', Search.as_view(), name='search'),
     path('add-rating/', AddStarRating.as_view(), name='add_rating'),
     path("json-filter/", JsonFilterMoviesView.as_view(), name='json_filter'),
     path('<slug:slug>/', MovieDetailView.as_view(), name="movie_detail"),
